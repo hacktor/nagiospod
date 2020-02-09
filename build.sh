112e758cd6de1a2e533b46d23eb0457541897494
Ruben de Groot <devnull@hacktor.com>
2020-02-09 12:36:37 +0100

    Added sendtelegram script to the build

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ buildah config --created-by "HackTor IT"  $newcontainer
 buildah config --author "Ruben de Groot" --label name=nagios $newcontainer
 buildah copy $newcontainer ./install.sh /usr/bin/install.sh
 buildah copy $newcontainer ./entrypoint.sh /usr/bin/entrypoint.sh
+buildah copy $newcontainer ./sendtelegram /usr/bin/sendtelegram
 buildah copy $newcontainer ./debconf /root/debconf
-buildah run $newcontainer chmod +x /usr/bin/install.sh /usr/bin/entrypoint.sh
+buildah run $newcontainer chmod +x /usr/bin/install.sh /usr/bin/entrypoint.sh /usr/bin/sendtelegram
 buildah copy $newcontainer ./nagios3 /tmp/nagios3
 buildah run $newcontainer /usr/bin/install.sh
 buildah config --entrypoint /usr/bin/entrypoint.sh $newcontainer
