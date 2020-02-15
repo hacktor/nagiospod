14741a837dc03b71b43771fe162b0c9f1de336c6
Ruben de Groot <devnull@hacktor.com>
2020-02-15 21:22:18 +0100

    Forgot sendsnail script :(

--- build.sh
+++ build.sh
@@ -10,8 +10,9 @@ buildah config --author "Ruben de Groot" --label name=nagiospod $newcontainer
 buildah copy $newcontainer ./install.sh /usr/bin/install.sh
 buildah copy $newcontainer ./entrypoint.sh /usr/bin/entrypoint.sh
 buildah copy $newcontainer ./sendtelegram /usr/bin/sendtelegram
+buildah copy $newcontainer ./sendsnail /usr/bin/sendsnail
 buildah copy $newcontainer ./debconf /root/debconf
-buildah run $newcontainer chmod +x /usr/bin/install.sh /usr/bin/entrypoint.sh /usr/bin/sendtelegram
+buildah run $newcontainer chmod +x /usr/bin/install.sh /usr/bin/entrypoint.sh /usr/bin/sendtelegram /usr/bin/sendsnail
 buildah copy $newcontainer ./nagios3 /tmp/nagios3
 buildah copy $newcontainer ./lib /tmp/lib
 buildah run $newcontainer /usr/bin/install.sh
