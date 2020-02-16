94b831d67f08ec515084937f47ccda895546d605
Ruben de Groot <devnull@hacktor.com>
2020-02-16 19:57:33 +0100

    Add some ENV

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # assume default cni pod network
-# todo add $USER3$ and $USER4$ macro's to resource.cfg
+CNI_NET=10.88.0.0/16
+GATEWAY=10.88.0.1
 
 newcontainer=$(buildah from ubuntu:bionic)
 
 buildah config --created-by "HackTor IT"  $newcontainer
 buildah config --author "Ruben de Groot" --label name=nagiospod $newcontainer
+buildah config --env CNI_NET=${CNI_NET} --env GATEWAY=${GATEWAY}
 buildah copy $newcontainer ./install.sh /usr/bin/install.sh
 buildah copy $newcontainer ./entrypoint.sh /usr/bin/entrypoint.sh
 buildah copy $newcontainer ./sendtelegram /usr/bin/sendtelegram
@@ -16,5 +18,6 @@ buildah run $newcontainer chmod +x /usr/bin/install.sh /usr/bin/entrypoint.sh /u
 buildah copy $newcontainer ./nagios3 /tmp/nagios3
 buildah copy $newcontainer ./lib /tmp/lib
 buildah run $newcontainer /usr/bin/install.sh
+buildah config --port 80 $newcontainer
 buildah config --entrypoint /usr/bin/entrypoint.sh $newcontainer
 buildah commit $newcontainer nagiospod:latest
